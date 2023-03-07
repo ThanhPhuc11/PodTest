d00e3dace3ccbc184009f1e4306f3396f4a7cc57
phucmt <thanhphuc.mai@n2nconnect.com>
2023-03-07 14:49:58 +0700

    hi

---
+++ PodTest/Hello.h
@@ -0,0 +1,18 @@
+//
+//  Hello.h
+//  PodTest
+//
+//  Created by PhucMT on 07/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Hello : NSObject
+
+- (NSString *)hello;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PodTest/Hello.m
@@ -0,0 +1,16 @@
+//
+//  Hello.m
+//  PodTest
+//
+//  Created by PhucMT on 07/03/2023.
+//
+
+#import "Hello.h"
+
+@implementation Hello
+
+- (NSString *)hello {
+    return @"Hello, world!";
+}
+
+@end
